b139cfb0c87cc8cb75fd44fae90ec519bfb341f6
dhimansapient <dhiman.sapient@gmail.com>
2025-02-06 21:52:15 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#tags="--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Ec2-Instance-Launched-via-Jenkins}]' 'ResourceType=volume,Tags=[{Key=Name,Value=Ec2-Instance-Launched-via-Jenkins}]'"
-
-aws ec2 run-instances --image-id ami-04b4f1a9cf54c11d0 --count 1 --instance-type t2.micro \
---key-name jenkins-test-2025 --security-group-ids sg-0f4644f4e87bea42b --subnet-id subnet-8630a0cb \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Production1}]'
+aws ec2 run-instances --image-id ami-04b4f1a9cf54c11d0 --count 1 --instance-type t2.micro --key-name jenkins-test-2025 --security-group-ids sg-0f4644f4e87bea42b --subnet-id subnet-8630a0cb --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Production1}]'
