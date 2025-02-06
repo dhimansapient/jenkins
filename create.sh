060cedfd79f4924dc4776c445e8a6658d9fd17d3
dhimansapient <dhiman.sapient@gmail.com>
2025-02-06 21:07:45 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-04b4f1a9cf54c11d0 --count 1 --instance-type t2.micro --key-name jenkins-test-2025 --security-group-ids sg-0f4644f4e87bea42b --region us-east-1
+aws ec2 run-instances --image-id ami-04b4f1a9cf54c11d0 --count 1 --instance-type t2.micro --key-name jenkins-test-2025 --security-group-ids sg-0f4644f4e87bea42b --subnet-id subnet-8630a0cb
