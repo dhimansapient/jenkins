a08c0f1832407a341ba1087d1f3010655f673958
dhimansapient <dhiman.sapient@gmail.com>
2025-02-06 19:34:05 +0530

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws ec2 run-instances --image-id ami-04b4f1a9cf54c11d0 --count 1 --instance-type t2.micro --key-name jenkins-test-2025 --security-groups-ids sg-0f4644f4e87bea42b --region us-east-1
